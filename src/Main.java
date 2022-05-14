d18e098fa647806e291d13c6956340f9aa47ba58
Volkan20 <volkandereli1@gmail.com>
2022-05-14 12:02:09 -0400

    2nd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Volkan");
+        System.out.println("Dereli");
+        System.out.println("No middle Name");
+        String name = scan.nextLine();
+        System.out.println(name);
     }
 }
